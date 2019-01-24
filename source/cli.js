ace9ef20486ecd0b4f6eefbdfeacbbaa1bb6075b
Arnelle Balane <arnellebalane@gmail.com>
2019-01-24 14:14:35 +0800

    Colorize cli help text using chalk

--- source/cli.js
+++ source/cli.js
@@ -1,13 +1,18 @@
 #!/usr/bin/env node
 import '@babel/polyfill';
 import meow from 'meow';
+import chalk from 'chalk';
 import openImage from '.';
 
 const cli = meow(`
-    Usage:
-      $ open-image <image-path>
+    ${chalk.dim.underline('Usage:')}
+      ${chalk.dim('$')} ${chalk.green('open-image')} ${chalk.yellow('<image-path>')}
 
-    <image-path> can be a filesystem path or url to the image
+    ${chalk.yellow('<image-path>')} ${chalk.dim('can be a filesystem path or url to the image')}
+
+    ${chalk.dim.underline('Examples:')}
+      ${chalk.dim('$')} ${chalk.green('open-image')} ${chalk.yellow('sample-image.jpg')}
+      ${chalk.dim('$')} ${chalk.green('open-image')} ${chalk.yellow('http://site.com/image.jpg')}
 `);
 
 const imagePath = cli.input[0];
